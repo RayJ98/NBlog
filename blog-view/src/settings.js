ff82db6d00d1f9bfa877a1151de42a0746de3c6c
leijian <@sw-leijian>
2025-02-28 14:21:13 +0800

    首页图片

--- blog-view/src/settings.js
+++ blog-view/src/settings.js
@@ -3,13 +3,13 @@ module.exports = {
 	 * @type {string}
 	 * @description 首页三张背景图
 	 */
-	bg1: 'https://cdn.naccl.top/blog/img/bg1.jpg',
-	bg2: 'https://cdn.naccl.top/blog/img/bg2.jpg',
-	bg3: 'https://cdn.naccl.top/blog/img/bg3.jpg',
+	bg1: 'http://rayj-upyun.test.upcdn.net/view/1.jpg',
+	bg2: 'http://rayj-upyun.test.upcdn.net/view/2.jpg',
+	bg3: 'http://rayj-upyun.test.upcdn.net/view/3.jpg',
 
 	/**
 	 * @type {string}
 	 * @description 首页故障风文字
 	 */
-	malfunctionText: 'Naccl\'s Blog'
+	malfunctionText: 'RayJ\'s Blog'
 }
